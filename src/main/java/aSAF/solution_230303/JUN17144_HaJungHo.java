ff147e5983447a8667426eadabe5417571331019
jumgho <103169947+jumgho@users.noreply.github.com>
2023-03-05 01:43:13 +0900

    23.03.05

--- src/main/java/aSAF/solution_230303/JUN17144_HaJungHo.java
+++ src/main/java/aSAF/solution_230303/JUN17144_HaJungHo.java
@@ -1,17 +1,206 @@
-package aSAF.solution_230303;
+    package aSAF.solution_230303;
+    
+    import java.io.*;
+    import java.util.*;
+    
+    public class JUN17144_HaJungHo {
+        static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        static StringTokenizer st;
+    
+        /*
+        ### 미세먼지 안녕!
+        - RxC 집 격자판
+        - 미세먼지
+            - 위치: (r,c), A 만큼 존재
+            - 사방 확산
+            - 공기청정기가 있거나, 칸이 없으면 확산 안함
+            - 확산의 양: A/5 한 만큼, 소수점 버림(int)
+            - 남은 양: A - (A/5 * 확산된 방향)
+            - 확산으로 겹치는 부분 주의
+        - 공기청정기
+            - 1열 설치, 2행 차지(세로)
+            - 공기청정기 위치에는 미세먼지가 없음
+            - 공기청정기 윗 부분은 반시계방향 바람 순환, 아랫 부분은 시계 방향 바람 순환(즉, 중간을 가로지는 바람이 나가고 끝에서 양끝으로 갈라지는 바람)
+            - 바람의 영향 : 미세먼지가 바람의 방향대로 1칸 이동
+            - 공기청정기로 들어온 미세먼지는 제거(정화)
+            - 바람이 안닿는 곳은 그대로 유지된다.
+        - T초 후 미세먼지 양 구하기
+    
+        ### 문제 해결
+        1. 미세먼지 확산 파트
+            - bfs, 입력받을 때부터 큐에 넣기
+        2. 공기청정기 바람 파트
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
-public class JUN17144_HaJungHo {
-    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-
-    /*
-    ### 미세먼지 안녕!
-     */
-}
+        ### 놓쳤던 부분들
+        - 동시성
+            - 확산이 동시에 이루어지는 것처럼 구현해야하는 것을 놓쳤음.
+            - 만약 미세먼지가 인접해있으면, 인접한 미세먼지가 모두 확산된 후 값이 변해야한다
+            => 해결: 가짜 맵 하나 더 구현
+         */
+    
+        static int R, C, T; // R: 세로, C: 가로, T: 시간(사이클 횟수)
+        static int[][] house;
+        static int[][] temp; // 동시에 확산되는 환경을 구현하기 위한 배열
+    //    static boolean[][] visited; // 방문 여부는 필요없을듯하다. 방문한 곳에 또 확산가능
+        static Queue<int[]> queue = new ArrayDeque<>(); // 세로 좌표, 가로 좌표, 미세먼지 양, 시간
+        static int[] dr = new int[]{1,0,-1,0};
+        static int[] dc = new int[]{0,1,0,-1};
+        static ArrayList<int[]> cleaner = new ArrayList<>(); // 공기청정기 좌표 저장
+    
+        public static void main(String[] args) throws IOException {
+            st = new StringTokenizer(bf.readLine());
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+            T = Integer.parseInt(st.nextToken());
+            house = new int[R+1][C+1];
+            temp = new int[R+1][C+1];
+    
+            for (int i = 1; i < R+1; i++) {
+                st = new StringTokenizer(bf.readLine());
+                for (int j = 1; j < C+1; j++) {
+                    house[i][j] = Integer.parseInt(st.nextToken());
+                    if ( house[i][j] == -1){ // 공기청정기 좌표 저장
+                        cleaner.add(new int[]{i, j});
+                    }
+                    else if (house[i][j] != 0){ // 미세먼지 좌표 저장
+                        queue.offer(new int[]{i, j, house[i][j]}); // 미세먼지 좌표, 미세먼지 양
+                    }
+                }
+            }
+            //확산 1회 + 바람 이동 1회가 한 세트
+            for (int t = 0; t < T; t++) {
+                diffusion();
+                airFlow();
+                for (int i = 0; i < R + 1; i++) {
+                    for (int j = 0; j < C + 1; j++) {
+                        queue.offer(new int[]{i, j, house[i][j]});
+                    }
+                }
+            }
+            bw.write(howMuch()+"");
+            bw.flush();
+            bw.close();
+        }
+    
+        private static int howMuch() {
+            int amount = 0;
+            for (int i = 1; i < R+1; i++) {
+                for (int j = 1; j < C+1; j++) {
+                    if(house[i][j] != -1 && house[i][j] != 0) amount += house[i][j];
+                }
+            }
+            return amount;
+        }
+    
+        private static void diffusion() {
+            while(!queue.isEmpty()){
+                int[] current = queue.poll();
+                int cr = current[0];
+                int cc = current[1];
+                int dusts = current[2];
+    
+                for (int i = 0; i < 4; i++) {
+                    int nr = cr + dr[i];
+                    int nc = cc + dc[i];
+                    if(nr<1 || nc<1 || nr>R || nc>C || house[nr][nc] == -1) continue;
+                    if(current[2]>=5){//확산이 가능한 최소 미세먼지양
+                        int separated = (int) current[2] / 5;
+                        temp[nr][nc] += separated; // 중첩 미세먼지
+                        dusts -= separated;
+                    }
+                }
+                temp[cr][cc] += dusts; // 확산되고 남은 미세먼지
+            }
+            for (int i = 0; i < R+1; i++) {
+                for (int j = 0; j < C+1; j++) {
+                    if (house[i][j] == -1) continue;
+                    house[i][j] = temp[i][j];
+                    temp[i][j] = 0;
+                }
+            }
+        }
+    
+        private static void airFlow() {
+            int[] fr = new int[]{-1, 0, 1, 0}; // 상, 우, 하, 좌
+            int[] fc = new int[]{0, 1, 0, -1};
+            int direction = 1;
+    
+            Queue<int[]> upperAir = new ArrayDeque<>();
+            Queue<int[]> lowerAir = new ArrayDeque<>();
+    
+            //윗 공기 순환 시작점
+            int ur = cleaner.get(0)[0];
+            int uc = cleaner.get(0)[1]+1;
+            upperAir.offer(new int[]{ur,uc,house[ur][uc]}); // 시작 지점 좌표 + 미세먼지양
+            house[ur][uc] = 0;
+            //아랫 공기 순환 시작점
+            int lr = cleaner.get(1)[0];
+            int lc = cleaner.get(1)[1]+1;
+            lowerAir.offer(new int[]{lr,lc,house[lr][lc]}); // 시작 지점 좌표 + 미세먼지양
+            house[lr][lc] = 0;
+            //윗 공기 순환 로직
+            while(!upperAir.isEmpty()){
+                int[] current = upperAir.poll();
+                int cr = current[0];
+                int cc = current[1];
+                int dusts = current[2];
+    
+                int nr = cr + fr[direction];
+                int nc = cc + fc[direction];
+                if(nc > C) {
+                    direction = 0; // 우측을 넘은 경우
+                }
+                else if(nr < 1) {
+                    direction = 3; // 위쪽을 넘은 경우
+                }
+                else if(nc < 1) {
+                    direction = 2; // 왼쪽을 넘은 경우
+                }
+                else if(nr > R) {
+                    direction = 1; // 아래쪽을 넘은 경우
+                }
+                nr = cr + fr[direction];
+                nc = cc + fc[direction];
+                if(house[nr][nc] != -1){
+                    upperAir.offer(new int[]{nr,nc,house[nr][nc]});
+                    house[nr][nc] = dusts;
+                }
+            }
+            direction = 1;
+            // 아랫 공기 순환 로직
+            while(!lowerAir.isEmpty()){
+                int[] current = lowerAir.poll();
+                int cr = current[0];
+                int cc = current[1];
+                int dusts = current[2];
+    
+                int nr = cr + fr[direction];
+                int nc = cc + fc[direction];
+                if(nc > C) {
+                    direction = 2; // 우측을 넘은 경우
+                }
+                else if(nr < 1) {
+                    direction = 1; // 위쪽을 넘은 경우
+                }
+                else if(nc < 1) {
+                    direction = 0; // 왼쪽을 넘은 경우
+                }
+                else if(nr > R) {
+                    direction = 3; // 아래쪽을 넘은 경우
+                }
+                nr = cr + fr[direction];
+                nc = cc + fc[direction];
+                if(house[nr][nc] != -1){
+                    lowerAir.offer(new int[]{nr,nc,house[nr][nc]});
+                    house[nr][nc] = dusts;
+                }
+            }
+    //        System.out.println("공기청정기");
+    //        for (int i = 0; i < R+1; i++) {
+    //            System.out.println(Arrays.toString(house[i]));
+    //        }
+    //        System.out.println();
+        }
+    
+    }
--- src/main/java/study/JUN17070.java
+++ src/main/java/study/JUN17070.java
@@ -18,9 +18,9 @@ public class JUN17070 {
         - 가로 상황 : 우, 우하단
         - 세로 상황 : 하단, 우하단
         - 대각선 상황 : 우, 우하단, 하단
-    3. 첫 시작은 무조건 1,1 에서 1,2
+    3. 첫 시작은 무조건 1,1 에서 1,2 => 가로 상황
     + 놓친 부분
-    + 4. 진행 방향에 따라 벽이 무조건 없어야하는 부분이 존재함. 체크할 것
+    + 4. 진행 방향에 따라, 즉 상황별로 벽이 무조건 없어야하는 부분이 존재함. 체크할 것
 
     - 목적
     => 1,1 시작으로 N, N 도착 가능한 경우의 수
@@ -28,18 +28,23 @@ public class JUN17070 {
     - 문제 해결
         - dfs 선택: N, N에 도달하면 cnt++ && 방문 여부 체크 false로 변경하여 다시 회귀
 
+    - 개선 방향
+    1. 최적화
+        - 최고점 || 메모리: 12904kb, 시간: 68ms
+        - 나 || 메모리: 18728kb, 시간: 348ms...ㅋ
      */
     static int N; // 집의 크기
     static int[][] house; // 집 배열
     static boolean[][] visited; // 집 배열 방문 여부
+
+    // 진행 방향 인덱스 || 0: 가로, 1: 대각선, 2: 세로
     static int[] dx = new int[]{1, 1, 0}; //가로축 이동 방향
     static int[] dy = new int[]{0, 1, 1}; //세로축 이동 방향
-//    static int[] direction = new int[]{0, 1, 2}; //0: 가로, 1: 대각선, 2: 세로
-    static int cnt;
+    static int cnt; // 도착 지점에 도달하는 경우의 수 카운팅
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(bf.readLine());
-        house = new int[N + 1][N + 1]; // 인덱스 1부터 시작, 인덱스 0에 닿으면 아웃
+        house = new int[N + 1][N + 1]; // 인덱스 1부터 시작, 인덱스 0에 닿으면 아웃(근데 사실 어차피 안 닿음)
         visited = new boolean[N + 1][N + 1];
         cnt = 0;
         for (int i = 1; i < N + 1; i++) {
@@ -48,27 +53,27 @@ public class JUN17070 {
                 house[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dfs(1, 2, 0);
+        dfs(1, 2, 0); // 1,2 좌표를 시작 지점 + 진행 방향 == 가로(0)
         bw.write(cnt + "");
         bw.flush();
         bw.close();
     }
 
-    private static void dfs(int y, int x, int direction) {
-        if (x == N && y == N) {
+    private static void dfs(int y, int x, int direction) { // y축 좌표, x축 좌표, 방향 제공
+        if (x == N && y == N) { // N, N 좌표 도달 상황: cnt 카운팅 및 재귀 종료
             cnt++;
             return;
         }
-        visited[y][x] = true;
+        visited[y][x] = true; // 길찾기 시작 지점 방문 체크(== 파이프 진행 방향 쪽 끝 지점)
 
-        switch (direction) {
+        switch (direction) { // 상황별 진행 방향
             //가로 상황
             case 0:
-                for (int i = 0; i < 2; i++) {
-                    int nx = x + dx[i];
+                for (int i = 0; i < 2; i++) { // 우, 우하단 가능 (0, 1)
+                    int nx = x + dx[i]; // 방향 탐색
                     int ny = y + dy[i];
-                    if(nx<=0 || nx > N || ny <=0 || ny > N) continue;
-                    if(isWall(i, y, x)) continue; // 상황별 벽 대응
+                    if(nx<=0 || nx > N || ny <=0 || ny > N) continue; // 선넘는 방향은 제외
+                    if(isWall(i, y, x)) continue; // 상황별 벽 가능 여부 대응, 좋지않은 위치에 벽이 있으면 제외
                     if(!visited[ny][nx]){
                         dfs(ny, nx, i);
                         visited[ny][nx] = false; // 이전 분기점으로 회귀하면서 재방문을 위한 false 변경
@@ -77,7 +82,7 @@ public class JUN17070 {
                 break;
             //대각선 상황
             case 1:
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 3; i++) { // 우, 우하단, 하단 가능 (0, 1, 2)
                     int nx = x + dx[i];
                     int ny = y + dy[i];
                     if(nx<=0 || nx > N || ny <=0 || ny > N) continue;
@@ -90,7 +95,7 @@ public class JUN17070 {
                 break;
             //세로 상황
             case 2:
-                for (int i = 1; i < 3; i++) {
+                for (int i = 1; i < 3; i++) { // 우하단, 하단 가능 (1, 2)
                     int nx = x + dx[i];
                     int ny = y + dy[i];
                     if(nx<=0 || nx > N || ny <=0 || ny > N) continue;
