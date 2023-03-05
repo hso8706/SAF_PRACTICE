2042f36f1aec9ead34d820964d063edc5c207cfb
jumgho <103169947+jumgho@users.noreply.github.com>
2023-03-05 13:10:10 +0900

    23.03.05

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -4,21 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int n;
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            for (int j = a; j <= b; j++) {
-
-            }
-        }
-
-
+    public static void main(String[] args) {
+        System.out.println("h");
     }
 }
