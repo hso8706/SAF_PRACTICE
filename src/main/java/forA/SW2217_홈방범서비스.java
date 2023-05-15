f83f3320494693367a065b7d7deb2cb1752c535a
Ha Jung Ho <ggk14@naver.com>
2023-05-15 17:33:53 +0900

    23.05.15

--- src/main/java/forA/SW2217_홈방범서비스.java
+++ src/main/java/forA/SW2217_홈방범서비스.java
@@ -10,6 +10,7 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class SW2217_홈방범서비스 {
+
     static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
@@ -22,6 +23,7 @@ public class SW2217_홈방범서비스 {
     서비스 영역 : bfs로 증가, 손해 범위에서 종료
      */
     static class Pair {
+
         int x;
         int y;
         int k; // 서비스 영역 단계 확인
@@ -35,17 +37,18 @@ public class SW2217_홈방범서비스 {
 
     static int T, N, M; //M: 하나의 집이 지불하는 비용
     static int[][] map;
-    static boolean[][] visited;
+    static boolean[][][] visited;
     static int expense;
     static int incomeBase;
     static int houses;
-    static int[] dx = new int[]{1,-1,0,0};
-    static int[] dy = new int[]{0,0,1,-1};
+    static int[] dx = new int[]{ 1, -1, 0, 0 };
+    static int[] dy = new int[]{ 0, 0, 1, -1 };
 
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(bf.readLine());
-        for (int t = 1; t < T+1; t++) {
-            bw.write("#"+t+" ");
+        for (int t = 1; t < T + 1; t++) {
+            bw.write("#" + t + " ");
+            houses = 0;
 
             st = new StringTokenizer(bf.readLine());
             N = Integer.parseInt(st.nextToken());
@@ -53,12 +56,18 @@ public class SW2217_홈방범서비스 {
             map = new int[N][N];
 
             for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(bf.readLine());
                 for (int j = 0; j < N; j++) {
-                    visited = new boolean[N][N];
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    visited = new boolean[2][N][N];
                     expense = 0;
                     incomeBase = 0;
-                    houses = 0;
-                    securityService(new Pair(i,j,1));
+                    securityService(new Pair(i, j, 1));
                 }
             }
             bw.write(houses + "\n");
@@ -70,35 +79,54 @@ public class SW2217_홈방범서비스 {
     private static void securityService(Pair start) {
         Deque<Pair> queue = new ArrayDeque<>();
         queue.offer(start);
-        visited[start.x][start.y] = true;
+        visited[0][start.x][start.y] = true;
+        visited[1][start.x][start.y] = true;
         int flag = 1;
-        expense++;
-        incomeBase += map[start.x][start.y];
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Pair current = queue.poll();
             int cx = current.x;
             int cy = current.y;
             int ck = current.k;
-            if(flag != ck){ //범위 확장이 종료된 시점, 범위 변화가 일어난 상황
-                flag = ck; //flag 갱신
-                int profit = (incomeBase*M) - expense;
-                if(profit > 0){ // 이익인 시점
+//            System.out.println(visited[1]);
+
+            if (flag < ck) { //범위 확장이 종료된 시점, 범위 변화가 일어난 상황
+
+                expense = flag * flag + (flag - 1) * (flag - 1);
+                incomeBase = contained();
+
+                int profit = (incomeBase * M) - expense;
+                if (profit > 0) { // 이익인 시점
                     houses = Math.max(houses, incomeBase); // houses 갱신
                 }
+                flag = ck; //flag 갱신
             }
+
+            visited[1][cx][cy] = true;
 
             for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx<0 || ny<0 || nx>=N || ny>=N) continue;
-                if(!visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    expense++;
-                    incomeBase += map[nx][ny];
-                    queue.offer(new Pair(nx, ny, ck+1));
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+                    continue;
+                }
+                if (!visited[0][nx][ny]) {
+                    visited[0][nx][ny] = true;
+                    queue.offer(new Pair(nx, ny, ck + 1));
                 }
             }
         }
+    }
+
+    private static int contained() {
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[1][i][j]) {
+                    cnt += map[i][j];
+                }
+            }
+        }
+        return cnt;
     }
 }
---
+++ src/main/java/implementation_and_bruteForce/JUN2504_괄호의값.java
@@ -0,0 +1,25 @@
+package implementation_and_bruteForce;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class JUN2504_괄호의값 {
+
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static char[] brackets;
+
+    public static void main(String[] args) throws IOException {
+        brackets = bf.readLine().toCharArray();
+        for (int i = 0; i < brackets.length; i++) {
+
+        }
+    }
+
+}
