e6322d2244fd456603616a2ab343808a2503163e
Ha Jung Ho <ggk14@naver.com>
2023-05-21 00:20:08 +0900

    23.05.21

---
+++ src/main/java/implementation_and_bruteForce/JUN17135_캐슬디펜스.java
@@ -0,0 +1,29 @@
+package implementation_and_bruteForce;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class JUN17135_캐슬디펜스 {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    /*
+    - N X M
+    - 성의 위치 : N+1
+    - 궁수 3명, 성의 위치 중 3 곳
+    - 궁수와 가장 가까운 적 제거(D 이하인 적 중에)
+        - 같은 거리면 왼쪽부터
+
+    - 궁수의 배치 : 임의, 제거할 수 있는 최대 적의 수 ==> 완탐
+     */
+    static int N, M, D;
+    static int[][] map
+    public static void main(String[] args) {
+
+
+    }
+}
