dd0a81194dafccd67505dcdc0af4ef9e785c83eb
Ha Jung Ho <ggk14@naver.com>
2023-09-08 18:19:14 +0900

    23.09.08

--- src/main/java/solving/JUN1270_전쟁땅따먹기.java
+++ src/main/java/solving/JUN1270_전쟁땅따먹기.java
@@ -5,6 +5,8 @@
     import java.io.IOException;
     import java.io.InputStreamReader;
     import java.io.OutputStreamWriter;
+    import java.sql.Wrapper;
+    import java.util.ArrayList;
     import java.util.Arrays;
     import java.util.StringTokenizer;
 
@@ -14,40 +16,58 @@
         static StringTokenizer st;
 
         static int n, Ti;
-        static int[] Nij;
+        static double[] Nij;
 
         public static void main(String[] args) throws IOException {
-    //        System.out.println(Math.pow(2, 31)*2 <= Integer.MAX_VALUE);
+//            System.out.println(Integer.parseInt(String.valueOf(Math.pow(2, 31))));
             n = Integer.parseInt(bf.readLine());
 
             external: for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(bf.readLine());
                 Ti = Integer.parseInt(st.nextToken());
-                Nij = new int[Ti];
+                if(Ti==0){
+                    bw.write("SYJKGW\n");
+                    continue;
+                }
+
+                Nij = new double[Ti];
 
                 for (int j = 0; j < Ti; j++) {
-                    Nij[j] = Integer.parseInt(st.nextToken());
+                    Nij[j] = Double.parseDouble(st.nextToken());
                 }
 
                 Arrays.sort(Nij);
 
-                int cnt = 0;
-                int flag = Nij[0];
+                int cnt = 1;
+                double flag = Nij[0];
+                ArrayList<Double> tempList = new ArrayList<>();
                 for (int j = 1; j < Ti; j++) {
+                    if(tempList.contains(Nij[j])) continue;
                     if(Nij[j] != flag) {
                         flag = Nij[j];
-                        cnt = 0;
+                        cnt = 1;
                     }
                     else {
                         cnt++;
-                        if(cnt >= Ti/2) {
-                            System.out.println(Nij[j]);
-                            continue external;
+                        if(cnt >= (double) Ti/2) {
+                            tempList.add(Nij[j]);
+                            cnt = 0;
+//                            bw.write((int)Nij[j]+"\n");
+//                            continue external;
                         }
                     }
                 }
-                System.out.println("SYJKGW");
+
+                if(tempList.size()==1) {
+                    double result = tempList.get(0);
+                    int result1 = (int) result;
+                    bw.write(result1+"\n");
+                }
+                else bw.write("SYJKGW\n");
+
             }
+            bw.flush();
+            bw.close();
         }
 
     }
